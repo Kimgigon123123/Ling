3746bd5f4427be4b90e5982502e4d379f1d8f702
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-07-28 17:13:00 +0900

    132
    132

--- Ling/app/src/main/java/com/example/ling/store/basket/BasketActivity.java
+++ Ling/app/src/main/java/com/example/ling/store/basket/BasketActivity.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.ling.R;
+import com.example.ling.databinding.ActivityBasketBinding;
+import com.example.ling.databinding.ActivityChargeCashBinding;
 
 public class BasketActivity extends AppCompatActivity {
+
+    ActivityBasketBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basket);
+        binding= ActivityBasketBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imgvBefore.setOnClickListener(v->{
+            finish();
+        });
+
+
+
     }
 }
