8d4e62854f8d0294ffb3f4a247ea26a61dcebf0b
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-07-28 15:36:22 +0900

    수정

--- Ling/app/src/main/java/com/example/ling/store/StoreMyinfoActivity.java
+++ Ling/app/src/main/java/com/example/ling/store/StoreMyinfoActivity.java
@@ -3,6 +3,7 @@ package com.example.ling.store;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
