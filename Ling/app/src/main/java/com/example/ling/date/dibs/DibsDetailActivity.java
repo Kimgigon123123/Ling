5ee222023f20c39f98853b94c7ee8767102822c6
kvmhyvmvn <hm990803@gmail.com>
2023-08-03 01:13:17 +0900

    상세정보 레이아웃 추가

--- Ling/app/src/main/java/com/example/ling/date/dibs/DibsDetailActivity.java
+++ Ling/app/src/main/java/com/example/ling/date/dibs/DibsDetailActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ling.R;
 import com.example.ling.databinding.ActivityDibsDetailBinding;
+import com.example.ling.date.InfoFragment;
+import com.example.ling.date.MapFragment;
 
 
 public class DibsDetailActivity extends AppCompatActivity {
@@ -17,11 +20,24 @@ public class DibsDetailActivity extends AppCompatActivity {
         binding = ActivityDibsDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.tvInfo.setText("[INFO]");
-        binding.tvDetailinfo.setText("상세정보가 나온다");
-
         binding.imgvBefore.setOnClickListener(v -> {
             finish();
         });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_dibsdetail, new InfoFragment()).commit();
+
+        binding.btnInfo.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.ln_dibsdetail, new InfoFragment()).commit();
+        });
+
+        binding.btnMap.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.ln_dibsdetail, new MapFragment()).commit();
+        });
+
+//        if(binding.btnInfo.isChecked()) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.ln_detail, new InfoFragment()).commit();
+//        }else if(binding.btnMap.isChecked()) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.ln_detail, new MapFragment()).commit();
+//        }
     }
 }
