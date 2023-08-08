fd0883b85e71bb2aca24538552c6933c5b1c87d6
SanctusAlbino <ghk1998@naver.com>
2023-08-08 17:14:14 +0900

    게시판 조회까지 완료
    08/08

---
+++ Ling/app/src/main/java/com/example/ling/board/Board_WorryAdapter.java
@@ -0,0 +1,53 @@
+package com.example.ling.board;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ling.databinding.ItemRecvBoardUserBinding;
+
+import java.util.ArrayList;
+
+
+public class Board_WorryAdapter extends RecyclerView.Adapter<Board_WorryAdapter.ViewHolder> {
+    ArrayList<BoardVO> list;
+
+    public Board_WorryAdapter(ArrayList<BoardVO> list) {
+        this.list = list;
+    }
+
+    Context context;
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        ItemRecvBoardUserBinding binding = ItemRecvBoardUserBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder h, int i) {
+        h.binding.boardNum.setText(list.get(i).getId());
+        h.binding.boardTitle.setText(list.get(i).getTitle());
+        h.binding.boardWriter.setText(list.get(i).getWriter());
+        h.binding.boardDate.setText(list.get(i).getWritedate());
+        h.binding.boardCnt.setText(list.get(i).getReadcnt()+"");
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ItemRecvBoardUserBinding binding;
+        public ViewHolder(@NonNull ItemRecvBoardUserBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
