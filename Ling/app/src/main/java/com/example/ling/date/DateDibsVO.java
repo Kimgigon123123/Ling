e0a703dd242fffc04d378eba59b1297fc353fb6d
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-08-08 17:45:19 +0900

    코드 수정

---
+++ Ling/app/src/main/java/com/example/ling/date/DateDibsVO.java
@@ -0,0 +1,55 @@
+package com.example.ling.date;
+
+public class DateDibsVO {
+	private int dibs_id, date_id;
+	private String id, date_category_code, date_name, date_address;
+
+	public int getDibs_id() {
+		return dibs_id;
+	}
+
+	public void setDibs_id(int dibs_id) {
+		this.dibs_id = dibs_id;
+	}
+
+	public int getDate_id() {
+		return date_id;
+	}
+
+	public void setDate_id(int date_id) {
+		this.date_id = date_id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDate_category_code() {
+		return date_category_code;
+	}
+
+	public void setDate_category_code(String date_category_code) {
+		this.date_category_code = date_category_code;
+	}
+
+	public String getDate_name() {
+		return date_name;
+	}
+
+	public void setDate_name(String date_name) {
+		this.date_name = date_name;
+	}
+
+	public String getDate_address() {
+		return date_address;
+	}
+
+	public void setDate_address(String date_address) {
+		this.date_address = date_address;
+	}
+
+}
