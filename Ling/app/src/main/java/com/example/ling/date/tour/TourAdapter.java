78bb28547103239e44c7102fd7fd94f9d9d4cd36
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-08-09 17:50:52 +0900

    관심목록추가(미완성)

--- Ling/app/src/main/java/com/example/ling/date/tour/TourAdapter.java
+++ Ling/app/src/main/java/com/example/ling/date/tour/TourAdapter.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ling.R;
+import com.example.ling.common.CommonConn;
 import com.example.ling.databinding.ItemRecvTouractBinding;
 import com.example.ling.date.DateInfoVO;
 
@@ -47,9 +48,15 @@ public class TourAdapter extends RecyclerView.Adapter<TourAdapter.ViewHolder> {
                 if ((boolean) h.binding.imgvFav.getTag()) {
                     h.binding.imgvFav.setTag(false);
                     h.binding.imgvFav.setImageResource(R.drawable.ic_fav);
+
                 } else {
                     h.binding.imgvFav.setTag(true);
                     h.binding.imgvFav.setImageResource(R.drawable.ic_fav2);
+//                    CommonConn conn = new CommonConn(context, "date_insertdibs");
+//                    conn.addParamMap("date_id", list.get(i).getDate_id());
+//                    conn.addParamMap("date_category_code", list.get(i).getDate_category_code());
+//                    conn.onExcute((isResult, data) -> {
+//                    });
                 }
             }
         });
