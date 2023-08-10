ea704115b6c29e3b786cd9377016c71105e2f60c
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-08-10 09:20:36 +0900

    캘린더 중

--- Ling/app/src/main/java/com/example/ling/home/HomeFragment.java
+++ Ling/app/src/main/java/com/example/ling/home/HomeFragment.java
@@ -3,12 +3,14 @@ package com.example.ling.home;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.example.ling.calendar.CalendarActivity;
 import com.example.ling.databinding.FragmentHomeBinding;
 
@@ -28,6 +30,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
 
+        Glide.with(this).load("http://192.168.0.38/hanul/img//andimg.jpg").into(binding.imgvManProfile);
         binding.imgvPhoto.setOnClickListener(v -> {
             Intent intent = new Intent(getContext(), PhotoActivity.class);
             startActivity(intent);
@@ -38,10 +41,12 @@ public class HomeFragment extends Fragment {
             startActivity(intent);
         });
 
-        binding.imgvLocTracking.setOnClickListener(v -> {
-            Intent intent = new Intent(getContext(), LocTrackingActivity.class);
-            startActivity(intent);
+        binding.imgvManProfile.setOnClickListener(v -> {
+//            showDialog();
         });
+
+
+
 
         Date date = new Date();
 
@@ -71,6 +76,27 @@ public class HomeFragment extends Fragment {
 
         return binding.getRoot();
 
+
+    }
+
+    public void showDialog(){
+        String[] dialog_item = {"갤러리", "카메라", "기본이미지"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("사진 업로드 방식");
+        builder.setSingleChoiceItems(dialog_item, -1, (dialog, i) -> {
+            if(dialog_item[i].equals("갤러리")){
+                //갤러리 로직
+//                showGallery();
+            }else if(dialog_item[i].equals("카메라")){
+                //카메라 로직
+//                showCamera();
+            }else if(dialog_item[i].equals("기본이미지")){
+
+            }
+            dialog.dismiss();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
 }
--- Ling/app/src/main/java/com/example/ling/home/PhotoActivity.java
+++ Ling/app/src/main/java/com/example/ling/home/PhotoActivity.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
@@ -27,6 +28,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -59,23 +61,35 @@ public class PhotoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPhotoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Glide.with(this).load("http://192.168.0.28/hanul/img//andimg.jpg").into(binding.imageView);
+        Glide.with(this).load("http://192.168.0.28/hanul/img//andimg.jpg").into(binding.imgvElbumCamera);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
 
-        binding.btnCamera.setOnClickListener(v -> {
-            cameraDialog = new CameraDialog(this);
-            cameraDialog.show();
-            cameraDialog.findViewById(R.id.ln_camera).setOnClickListener(v2->{//imgv_photos               showGallery();
-                showCamera();
-            });
-            cameraDialog.findViewById(R.id.ln_photos).setOnClickListener(v2->{//imgv_photos               showGallery();
-                showGallery();
-            });
+
+        binding.imgvElbumCamera.setOnClickListener(v->{//imgv_photos               showGallery();
+            showCamera();
         });
+
+        binding.imgvFolderAdd.setOnClickListener(view -> {
+            AlertDialog.Builder follder = new AlertDialog.Builder(this);
+            follder.setTitle("생성할 폴더명");
+            final EditText name = new EditText(this);
+            follder.setView(name);
+            follder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) { //확인 버튼을 클릭했을때
+
+                }
+            });
+            follder.setNegativeButton("취소",new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) { //취소 버튼을 클ㅣ
+                }
+            });
+            follder.show();
+        });
+
 
 
     }
@@ -88,7 +102,7 @@ public class PhotoActivity extends AppCompatActivity {
             @Override
             public void onActivityResult(ActivityResult result) {
                 //액티비티(카메라 액티비티)가 종료되면 콜백으로 데이터를 받는 부분. (기존에는 onActivityResult메소드가 실행/ 현재는 해당 메소드)
-                Glide.with(PhotoActivity.this).load(camera_uri).into(binding.imageView);
+                Glide.with(PhotoActivity.this).load(camera_uri).into(binding.imgvElbumCamera);
                 File file = new File(getRealPath(camera_uri));
 
                 if(file!=null){
@@ -136,7 +150,7 @@ public class PhotoActivity extends AppCompatActivity {
             //갤러리 액티비티가 종료되었다. (사용자가 사진을 선택했는지)
             Log.d("갤러리", "onActivityResult: " + data.getData());
             Log.d("갤러리", "onActivityResult: " + data.getData().getPath());
-            Glide.with(this).load(data.getData()).into(binding.imageView); //갤러리 이미지가 잘 붙는지??
+            Glide.with(this).load(data.getData()).into(binding.imgvElbumCamera); //갤러리 이미지가 잘 붙는지??
             String img_path = getRealPath(data.getData());
 
             //MultiPart 형태로 전송 (File)
