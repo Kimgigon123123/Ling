4c31fb1625e4b531fa40f574d1ebdbd9151527d1
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-08-13 17:38:56 +0900

    주소,상세주소 90%

---
+++ Ling/app/src/main/java/com/example/ling/store/myinfo/AddressSearchActivity.java
@@ -0,0 +1,44 @@
+package com.example.ling.store.myinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.ling.R;
+
+public class AddressSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_search);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new BridgeInterface(),"Android");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:sample2_execDaumPostcode();");
+            }
+        });
+        // Android -> Javascript 함수 호출!
+        webView.loadUrl("http://192.168.0.36:8080/ling/address");
+    }
+
+    private class BridgeInterface {
+        @JavascriptInterface
+        public void processDATA(String data) {
+            // 다음(카카오) 주소 검색 API의 결과 값이 브릿지 통로를 통해 전달 받는다. (from Javscript)
+            Intent intent = new Intent();
+            intent.putExtra("data",data);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
+
+    }
+}
