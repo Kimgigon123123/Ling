db6fcdfb8f9fde3c7a1a65a2798b96b4cecc2915
ym <youngmoon525@naver.com>
2023-08-16 17:06:48 +0900

    바텀 네비 작업1
    1

--- Ling/app/src/main/java/com/example/ling/MainActivity.java
+++ Ling/app/src/main/java/com/example/ling/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.ling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,231 +34,118 @@ import devlight.io.library.ntb.NavigationTabBar;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    Fragment fragment;
-    BoardFragment boardFragment = new BoardFragment();
+
+    ArrayList<MainMenuDTO> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().replace(R.id.container, new HomeFragment()).commit();
-
+        initItem();
         initUI();
 
     }
 
+
+
+    //바텀 네비게이션 아이템 초기화 메소드 2023-08-16 kym
+    public void initItem(){
+        //                      붙일프래그먼트  ,        아이콘               ,   선택배경색상 ,    글씨  ,       뱃지보여줄꺼면 뱃지 그씨
+        list.add(new MainMenuDTO(new DateFragment() , R.drawable.btm_menu_1 ,"#123456" , "여행지" ,  "여행지 선택"  ));
+        list.add(new MainMenuDTO(new ChatFragment() , R.drawable.btm_menu_2 ,"#567891" , "채팅" ,  null ));
+        list.add(new MainMenuDTO(new HomeFragment() , R.drawable.btm_menu_3 ,"#FF8F1B" , "홈" ,  "홈 선택"  ));
+        list.add(new MainMenuDTO(new BoardFragment() , R.drawable.btm_menu_4 ,"#5FB59C" , "게시판" ,  null  ));
+        list.add(new MainMenuDTO(new StoreCoFragment() , R.drawable.btm_menu_5 ,"#847253" , "스토어" ,  "스토어 선택"  ));
+    }
+
+    //바텀 네비게이션 초기화 및 뷰페이저 초기화  ↗ 로직 끝나고 실행해야함  2023-08-16 kym
     private void initUI() {
-//        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
-//        viewPager.setAdapter(new PagerAdapter() {
-//            @Override
-//            public int getCount() {
-//                return 5;
-//            }
-//
-//            @Override
-//            public boolean isViewFromObject(final View view, final Object object) {
-//                return view.equals(object);
-//            }
-//
-//            @Override
-//            public void destroyItem(final View container, final int position, final Object object) {
-//                ((ViewPager) container).removeView((View) object);
-//            }
-//
-//            @Override
-//            public Object instantiateItem(final ViewGroup container, final int position) {
-//                final View view = LayoutInflater.from(
-//                        getBaseContext()).inflate(R.layout.item_vp, null, false);
-//
-//                final TextView txtPage = (TextView) view.findViewById(R.id.txt_vp_item_page);
-//                txtPage.setText(String.format("Page #%d", position));
-//
-//                container.addView(view);
-//                return view;
-//            }
-//        });
+       binding.pager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+           @NonNull
+           @Override
+           public Fragment getItem(int i) {
+               return list.get(i).fragment;
+           }
 
-       // final String[] colors = getResources().getStringArray(R.array.default_preview);
-        final String[] colors = { "#123456" , "#654321" , "#112233" , "#445566" , "#776655" };
-        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
+           @Override
+           public int getCount() {
+               return list.size();
+           }
+       });
+
         final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.travel),
-                        Color.parseColor(colors[0]))
-                        .selectedIcon(getResources().getDrawable(R.drawable.travel))
-                        .title("Heart")
-                        .badgeTitle("NTB")
+        for (int i = 0; i < list.size(); i++) {
+         NavigationTabBar.Model.Builder builder =   new NavigationTabBar.Model.Builder(
+                    getResources().getDrawable(list.get(i).defaultIcon),
+                    Color.parseColor(list.get(i).color))
+                    .title(list.get(i).title);
+            if(list.get(i).badgeTitle != null){
+                builder.badgeTitle(list.get(i).badgeTitle);
+            }
+            models.add(builder.build());
+        }
 
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_before),
-                        Color.parseColor(colors[1]))
-//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-                        .title("Cup")
-                        .badgeTitle("with")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_before),
-                        Color.parseColor(colors[2]))
-                        .selectedIcon(getResources().getDrawable(R.drawable.ic_before))
-                        .title("Diploma")
-                        .badgeTitle("state")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_before),
-                        Color.parseColor(colors[3]))
-//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-                        .title("Flag")
-                        .badgeTitle("icon")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        getResources().getDrawable(R.drawable.ic_before),
-                        Color.parseColor(colors[4]))
-                        .selectedIcon(getResources().getDrawable(R.drawable.ic_before))
-                        .title("Medal")
-                        .badgeTitle("777")
-                        .build()
-        );
+        binding.ntbHorizontal.setModels(models);
+        binding.ntbHorizontal.setViewPager(binding.pager, 2);
+        binding.ntbHorizontal.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
 
-        navigationTabBar.setModels(models);
-     //   navigationTabBar.setViewPager(viewPager, 2);
-//        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(final int position) {
-//                navigationTabBar.getModels().get(position).hideBadge();
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(final int state) {
-//
-//            }
-//        });
+            }
 
-        navigationTabBar.postDelayed(new Runnable() {
+            @Override
+            public void onPageSelected(final int position) {
+                binding.ntbHorizontal.getModels().get(position).hideBadge();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+
+            }
+        });
+
+        binding.ntbHorizontal.postDelayed(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
-                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
-                    navigationTabBar.postDelayed(new Runnable() {
+                for (int i = 0; i < binding.ntbHorizontal.getModels().size(); i++) {
+                    final NavigationTabBar.Model model = binding.ntbHorizontal.getModels().get(i);
+                    binding.ntbHorizontal.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            model.showBadge();
+
+                            Log.d("뱃지", "run: " + model.getBadgeTitle());
+                            if(model.getBadgeTitle()!=null && !model.getBadgeTitle().equals("0")) {
+
+                                model.showBadge();
+                            }
                         }
                     }, i * 100);
                 }
             }
         }, 500);
     }
-//        binding.navigation.setSelectedItemId(R.id.tab_home);
-//        binding.navigation.setBackground(null);
-//
-////        binding.mainFab.setOnClickListener(view -> {
-////            Intent intent = new Intent(MainActivity.this, CalendarActivity.class );
-////            startActivity(intent);
-////        });
-//
-//        binding.navigation.setItemIconTintList(null);
-//        binding.navigation.setOnItemSelectedListener(item -> {
-//
-//
-//            if(item.getItemId() == R.id.tab_travel){
-//                fragment = new DateFragment();
-//
-//            }else if(item.getItemId() == R.id.tab_chat){
-//                fragment = new ChatFragment();
-//
-//            }else if(item.getItemId() == R.id.tab_home){
-//
-//                fragment = new HomeFragment();
-//
-//            }else if(item.getItemId() == R.id.tab_store){
-//
-//                fragment = new StoreCoFragment();
-//
-//            }else if(item.getItemId() == R.id.tab_board){
-//                fragment = boardFragment;
-//
-//            }
-//            manager.beginTransaction().replace(R.id.container, fragment).commit();
-//
-//            return true;
-//        });
-//
-//        //store 반품처리
-//        Intent intent = getIntent();
-//        String str = intent.getStringExtra("return");
-//        if(str !=null && str.equals("return")){
-//            getSupportFragmentManager().beginTransaction().replace(R.id.container,new StoreCoFragment()).commit();
-//        }
-//
-//
-//    }
-//
-//    int backPressed = 0;
-//    public void changeFragment(int backPressed, Fragment fragment){
-//        this.backPressed = backPressed;
-//        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-//    }
-//    private long backKeyPressedTime = 0;
-//    @Override
-//    public void onBackPressed() {
-//        // 기존의 뒤로가기 버튼의 기능 제거
-//        // super.onBackPressed();
-//
-//        // 2000 milliseconds = 2 seconds
-//        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
-//            backKeyPressedTime = System.currentTimeMillis();
-//            Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
-//            changeFragment(0 , new HomeFragment() );
-//            return;
-//        }
-//
-//        // 2초 이내에 뒤로가기 버튼을 한번 더 클릭시 finish()(앱 종료)
-//        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
-//            //finish();
-//            finishAffinity();
-//            System.runFinalization();
-//            System.exit(0);
-//        }
-//
-//
-//    }
 
-//    @Override
-//    public void onBackPressed() {
-//        if(backPressed==1){
-//            changeFragment(0 , boardFragment );
-//        }else {
-//            finish();
-//        }
-//    }
-
-
-//
-//    getResources().getDrawable(R.drawable.ic_before),
-//                        Color.parseColor(colors[4]))
-//            .selectedIcon(getResources().getDrawable(R.drawable.ic_before))
-//            .title("Medal")
-//                        .badgeTitle("777")
-//                        .build()
     public class MainMenuDTO {
-        Fragment fragment;
+    public MainMenuDTO(Fragment fragment, int defaultIcon, int selectedIcon, String color, String title, String badgeTitle) {
+        this.fragment = fragment;
+        this.defaultIcon = defaultIcon;
+        this.selectedIcon = selectedIcon;
+        this.color = color;
+        this.title = title;
+        this.badgeTitle = badgeTitle;
+    }
+    public MainMenuDTO(Fragment fragment, int defaultIcon, String color, String title, String badgeTitle) {
+        this.fragment = fragment;
+        this.defaultIcon = defaultIcon;
+        this.color = color;
+        this.title = title;
+        this.badgeTitle = badgeTitle;
+    }
+    Fragment fragment;
+        int defaultIcon , selectedIcon ;
+        String  color , title , badgeTitle;
+
     }
 
 
