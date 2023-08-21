20d8a4577eae27392402524e5c4f9c760c4c244d
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-08-21 18:16:07 +0900

    약간수정

--- Ling/app/src/main/java/com/example/ling/capsule/CapsuleMainActivity.java
+++ Ling/app/src/main/java/com/example/ling/capsule/CapsuleMainActivity.java
@@ -2,15 +2,34 @@ package com.example.ling.capsule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.ling.R;
+import com.example.ling.databinding.ActivityCapsuleMainBinding;
+import com.example.ling.databinding.ActivityTimeCapsuleBinding;
+import com.example.ling.home.LocTrackingActivity;
 
 public class CapsuleMainActivity extends AppCompatActivity {
+
+    ActivityCapsuleMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_capsule_main);
+        binding=ActivityCapsuleMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnAdd.setOnClickListener(v->{
+            Intent intent = new Intent(this, CapsuleAddActivity.class);
+            startActivity(intent);
+        });
+
+        binding.imgvBefore.setOnClickListener(v->{
+            finish();
+        });
+
+
     }
 }
