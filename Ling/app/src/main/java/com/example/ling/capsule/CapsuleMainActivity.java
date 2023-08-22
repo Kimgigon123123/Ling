216bdc813ecc6b27183f10aa93825a0511865627
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-08-22 16:03:32 +0900

    타임캡슐90%

--- Ling/app/src/main/java/com/example/ling/capsule/CapsuleMainActivity.java
+++ Ling/app/src/main/java/com/example/ling/capsule/CapsuleMainActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.ling.R;
@@ -15,6 +16,8 @@ import com.example.ling.common.CommonVar;
 import com.example.ling.databinding.ActivityCapsuleMainBinding;
 import com.example.ling.databinding.ActivityTimeCapsuleBinding;
 import com.example.ling.home.LocTrackingActivity;
+import com.example.ling.store.ChargeVO;
+import com.example.ling.store.CompleteDialog;
 import com.example.ling.store.StoreCoAdater;
 import com.example.ling.store.storeCO.StoreCOVO;
 import com.google.gson.Gson;
@@ -51,6 +54,11 @@ public class CapsuleMainActivity extends AppCompatActivity {
             select();
         });
 
+        binding.imgvLeft.setOnClickListener(v->{
+            i=i-1;
+            select();
+        });
+
     }
 
     public void select() {
@@ -61,25 +69,78 @@ public class CapsuleMainActivity extends AppCompatActivity {
             ArrayList<CapsuleVO> list = new Gson().fromJson(data, new TypeToken<ArrayList<CapsuleVO>>() {
             }.getType());
 
-            binding.tvTitle.setText(list.get(i).getTc_title());
-            binding.tvDDay.setText(list.get(i).getD_day());
-            binding.tvDate.setText(list.get(i).getTc_date());
-            binding.tvIsopen.setText(list.get(i).getIsopen());
 
-            if(binding.tvIsopen.getText().equals("개봉가능")){
-                binding.tvIsopen.setTextColor(getColor(R.color.green));
+            if(list.isEmpty()){
+                binding.cdvCapsule.setVisibility(View.INVISIBLE);
+                binding.tvEmptyCapsule.setVisibility(View.VISIBLE);
+            }else{
+
+                binding.cdvCapsule.setVisibility(View.VISIBLE);
+                binding.tvEmptyCapsule.setVisibility(View.INVISIBLE);
+
+                if(i>=list.size()){
+                    i=0;
+                }
+
+                if(i<0){
+                    i=list.size()-1;
+                }
+
+                binding.tvTitle.setText(list.get(i).getTc_title());
+                binding.tvDDay.setText(list.get(i).getD_day());
+                binding.tvDate.setText(list.get(i).getTc_date());
+                binding.tvIsopen.setText(list.get(i).getIsopen());
+
+                if(list.get(i).tc_state.equals("OPEN")){
+                    binding.imgvCapsule.setImageResource(R.drawable.capsule_open);
+                    binding.cdvIsopen.setVisibility(View.INVISIBLE);
+                }else{
+                    binding.imgvCapsule.setImageResource(R.drawable.capsule);
+                    binding.cdvIsopen.setVisibility(View.VISIBLE);
+                }
+
+                if(binding.tvIsopen.getText().equals("개봉가능")){
+                    binding.tvIsopen.setTextColor(getColor(R.color.green));
+                }else{
+                    binding.tvIsopen.setTextColor(getColor(R.color.red));
+                }
+
+                binding.btnOpen.setOnClickListener(v->{
+                    if(binding.tvIsopen.getText().equals("개봉불가")){
+                        Toast.makeText(this, "아직 개봉이 불가능합니다", Toast.LENGTH_SHORT).show();
+                    }else{
+                        CommonConn conn2 = new CommonConn(this, "update_capsule_state");
+                        conn2.addParamMap("tc_no" , list.get(i).tc_no);
+                        conn2.onExcute((isResult2, data2) -> {
+
+                        });
+
+                        select();
+
+                        LetterDialog dialog = new LetterDialog(this,i);
+                        dialog.show();
+                    }
+                });
+
+                binding.btnDelete.setOnClickListener(v->{
+                    CommonConn conn2 = new CommonConn(this, "delete_capsule");
+                    conn2.addParamMap("tc_no" , list.get(i).tc_no);
+                    conn2.onExcute((isResult2, data2) -> {
+                        select();
+                    });
+                });
+
+
             }
 
-            binding.btnOpen.setOnClickListener(v->{
-                if(binding.tvIsopen.getText().equals("개봉불가")){
-                    Toast.makeText(this, "아직 개봉이 불가능합니다", Toast.LENGTH_SHORT).show();
-                }else{
-                    LetterDialog dialog = new LetterDialog(this,i);
-                    dialog.show();
-                }
-            });
 
         });
 
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        select();
     }
 }
